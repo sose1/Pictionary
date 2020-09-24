64313630f2e0149bca5151bb4074c919c6d9f69d
Kamil Wąsowski <kamilw2000@interia.eu>
2020-09-24 13:34:58 +0200

    + new design button

--- features/home/src/main/java/pl/sose1/home/HomeActivity.kt
+++ features/home/src/main/java/pl/sose1/home/HomeActivity.kt
@@ -1,8 +1,8 @@
 package pl.sose1.home
 
 import android.app.AlertDialog
-import org.koin.android.viewmodel.ext.android.viewModel
 import androidx.lifecycle.observe
+import org.koin.android.viewmodel.ext.android.viewModel
 import pl.sose1.base.view.BaseActivity
 import pl.sose1.home.databinding.ActivityHomeBinding
 
@@ -27,7 +27,7 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(layoutId =
         AlertDialog.Builder(this)
             .setTitle(R.string.dialog_title_create_room)
             .setPositiveButton(R.string.yes) { _, _ -> }
-            .setNegativeButton(R.string.no) { _, _ -> }
+            .setNegativeButton(android.R.string.cancel) { _, _ -> }
             .show()
     }
 }
